7506e43e6b6afddb836ef0a0e54bf2781f77d388
Juan Aracena <juanjo.aracenasantelises@gmail.com>
2025-03-22 19:30:53 -0400

    Implemented backend view for the login and logout pages

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -5,8 +5,23 @@ from user_profile.models import UserProfile
 from rest_framework.response import Response
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.utils.decorators import method_decorator
+from django.contrib import auth
 
 # Create your views here.
+
+@method_decorator(csrf_protect, name='dispatch')
+class CheckAuthenticatedView(APIView):
+    def get(self, request, format=None):
+        try:
+            isAuthenticated = User.is_authenticated
+
+            if isAuthenticated:
+                return Response({ 'isAuthenticated': 'success' })
+            else:
+                return Response({ 'isAuthenticated': 'error' })
+        except:
+            return Response({ 'error': 'Something went wrong when checking authentication status' })
+        
 @method_decorator(csrf_protect, name='dispatch')
 class SignupView(APIView):
     permission_classes = (permissions.AllowAny, )
@@ -18,24 +33,54 @@ class SignupView(APIView):
         password = data['password']
         re_password = data['re_password']
 
-        if password == re_password:
-            if User.objects.filter(username=username).exists():
-                return Response({ 'error': 'Username already exists' })
+        try:
+            if password == re_password:
+                    if User.objects.filter(username=username).exists():
+                        return Response({ 'error': 'Username already exists' })
+                    else:
+                        if len(password) < 6:
+                            return Response({ 'error': 'Password must be at least 6 characters' }) 
+                        else:
+                            user = User.objects.create_user(username=username, password=password)
+
+                            user = User.objects.get(id=user.id)
+
+                            user_profile = UserProfile.objects.create(user=user, first_name='', last_name='')
+
+                            return Response({ 'success': 'User created successfully' })
             else:
-                if len(password) < 6:
-                    return Response({ 'error': 'Password must be at least 6 characters' }) 
-                else:
-                    user = User.objects.create_user(username=username, password=password)
+                return Response({ 'error': 'Passwords do not match' })
+        except:
+                    return Response({ 'error': 'Something went wrong when registering account' })
 
-                    user = User.objects.get(id=user.id)
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny, )
 
-                    user_profile = UserProfile.objects.create(user=user, first_name='', last_name='')
+    def post(self, request, format=None):
+        data = self.request.data
 
-                    return Response({ 'success': 'User created successfully' })
+        username = data['username']
+        password = data['password']
 
-        else:
-            return Response({ 'error': 'Passwords do not match' })
+        try:
+            user = auth.authenticate(username=username, password=password)
 
+            if user is not None:
+                auth.login(request, user)
+                return Response({ 'success': 'User authenticated', 'username': username })
+            else:
+                return Response({ 'error': 'Error Authenticating' })
+        except:
+            return Response({ 'error': 'Something went wrong when logging in' })
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        try:
+            auth.logout(request)
+            return Response({ 'success': 'Logged Out'})
+        except:
+            return Response({'error': 'Something went wrong when logging out'})
+        
 @method_decorator(ensure_csrf_cookie, name='dispatch')     
 class GetCSRFToken(APIView):
     permission_classes = (permissions.AllowAny, )
